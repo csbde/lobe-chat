634ea93ecdde6f46405a8336a4c8716aaf30d1f4
SimonCHEN <csbdess@gmail.com>
2024-05-16 10:49:03 +0800

    feat: add support for URL query parameters

--- src/app/(loading)/Redirect.tsx
+++ src/app/(loading)/Redirect.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { memo, useEffect } from 'react';
 
 import { messageService } from '@/services/message';
 import { sessionService } from '@/services/session';
+import { useGlobalStore } from '@/store/global';
 
 const checkHasConversation = async () => {
   const hasMessages = await messageService.hasMessages();
@@ -14,6 +15,10 @@ const checkHasConversation = async () => {
 
 const Redirect = memo(() => {
   const router = useRouter();
+  // get settings str from query
+  const searchParams = useSearchParams();
+  const settings = searchParams.get('settings');
+  const [setConfig] = useGlobalStore((s) => [s.setModelProviderConfig]);
 
   useEffect(() => {
     checkHasConversation().then((hasData) => {
@@ -24,6 +29,25 @@ const Redirect = memo(() => {
       }
     });
   }, []);
+
+  useEffect(() => {
+    if (settings) {
+      try {
+        const json = JSON.parse(settings);
+        const { openAI } = json as {
+          openAI: {
+            OPENAI_API_KEY?: string;
+            endpoint?: string;
+          };
+        };
+        setTimeout(() => {
+          if (openAI) setConfig('openAI', openAI);
+        }, 5000);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, [settings]);
 
   return null;
 });
